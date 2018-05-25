4ca0ece2324725f401f942b3f4034358f27c45fb
zayrtun <zay.t@hotmail.com>
2018-05-25 15:43:57 +0630

    end

---
+++ debugging.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Debugging</title>
+</head>
+<body>
+
+  <?php
+    $number = 99;
+    $string = "Bug?";
+    $array = array(1 => "Homepage", 2 => "About Us", 3 => "Services");
+    
+    var_dump($number);
+    var_dump($string);
+    var_dump($array);
+  
+  ?>
+  <br />
+  <pre>
+  <?php
+    // print_r(get_defined_vars());
+  ?>
+  </pre>
+  <br />
+  <?php
+
+    function say_hello_to($word) {
+      echo "Hello {$word}!<br />";
+      var_dump(debug_backtrace());
+    }
+
+    say_hello_to('Everyone');
+  ?>
+  
+</body>
+</html>
